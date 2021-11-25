b9f88bb568287676192063bdf27095df64e7ec98
박재욱 <80030233+Mesopotamia2957@users.noreply.github.com>
2021-11-25 20:10:28 +0900

    Add files via upload

---
+++ app/src/main/java/com/caucap2021_1_2_10/ddubuk2/MypageActivity.java
@@ -0,0 +1,13 @@
+package com.caucap2021_1_2_10.ddubuk2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MypageActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
