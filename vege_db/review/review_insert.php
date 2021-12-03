2896d0aaa7b23ef59a6447991b5dbbc7ca44f9fc
leehyemie <80510889+leehyemie@users.noreply.github.com>
2021-12-03 10:50:17 +0900

    image upload

--- vege_db/review/review_insert.php
+++ vege_db/review/review_insert.php
@@ -1,10 +1,69 @@
 <?php
 // 리뷰 등록
     header("Content-Type: application/json; charset=utf-8");
-    include "../dbcon.php";
+    include "../dbcon.php"; // db 연결
+    //include "image_upload.php"; // 이미지 업로드
     
     $title = $_POST["title"]; // 제목
-    $body = $_POST["body"]; // 본문
+    $body = $_POST["body"]; // 본문    
+    
+    $tempFile = $_FILES['imgFile']['tmp_name']; // $tempFile="D:\xampp\tmp\phpA3BD.tmp" 
+    $fileTypeExt = explode("/", $_FILES['imgFile']['type']); //$fileTypeExt=[image, jpeg]
+    $fileType = $fileTypeExt[0]; //$fileType = image
+    $fileExt = $fileTypeExt[1]; //fileExt = jpeg
+    $extStatus = false;
+
+    // image 데이터 검증(크기, 확장자, 이미지파일, 개수(3개))
+    switch($fileExt){
+        case 'jpeg':
+        case 'jpg':
+        case 'gif':
+        case 'bmp':
+        case 'png':
+            $extStatus = true;
+            break;
+        
+        default:
+            echo "이미지 전용 확장자(jpg, bmp, gif, png)외에는 사용이 불가합니다."; 
+            exit;
+            break;
+    }
+
+    // 이미지 파일이 맞는지 검사. 
+    if($fileType == 'image'){
+        // 허용할 확장자를 jpg, bmp, gif, png로 정함, 그 외에는 업로드 불가
+        if($extStatus){
+            // 임시 파일을 옮길 디렉토리 및 파일명
+            $resFile = "../../img/{$_FILES['imgFile']['name']}";
+            $resFileName = $_FILES['imgFile']['name'];
+
+            // 임시 저장된 파일을 우리가 저장할 디렉토리 및 파일명으로 옮김
+            $imageUpload = move_uploaded_file($tempFile, $resFile);
+            
+            // 업로드 성공 여부 확인(json 오류 코드)
+            if($imageUpload == false){
+                $response["success"] = false;
+                $response["msg"] = "파일 업로드에 실패하였습니다.";
+                $response["errcode"] = 2050;
+                echo json_encode($response);
+                exit();
+            }
+        }	
+        else {
+            $response["success"] = false;
+            $response["msg"] = "파일 확장자는 jpg, bmp, gif, png 이어야 합니다.";
+            $response["errcode"] = 2051;
+            echo json_encode($response);
+            exit();
+        }	
+    }	
+    else {
+        $response["success"] = false;
+        $response["msg"] = "이미지 파일이 아닙니다.";
+        $response["errcode"] = 2052;
+        echo json_encode($response);
+        exit();
+    }
     
 
     // 제목, 본문 문자열 길이 제한
@@ -32,7 +91,8 @@
         exit();
     }
     
-    $sql1 = "INSERT INTO review (`title`, `body`) VALUES ('$title', '$body')";
+    $sql1 = "INSERT INTO review (`title`, `body`, `image`) VALUES ('$title', '$body', '$resFileName');";
+    echo "$sql1";
     $result = mysqli_query($con, $sql1);    
 
     if(!$result){
@@ -44,7 +104,7 @@
     }
 
     // num 구하기
-    $sql2 = "SELECT num FROM review ORDER BY `num` DESC LIMIT 1";
+    $sql2 = "SELECT num FROM review ORDER BY `num` DESC LIMIT 1"; // WHERE $title? 
     $result = mysqli_query($con, $sql2);
     // $data = array();
     // while($row = mysqli_fetch_assoc($result)) {
@@ -56,7 +116,7 @@
     
    
     // 리뷰 등록
-    $sql3 = "SELECT title, body FROM review WHERE `num` = $num";
+    $sql3 = "SELECT title, body FROM review WHERE `num` = $num"; 
     $result = mysqli_query($con, $sql3);
 
     $data = array();
