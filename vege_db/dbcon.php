ea3dc3932c0c2ae64a06e02ef21fa53de4060ce6
leehyemie <80510889+leehyemie@users.noreply.github.com>
2021-12-06 20:58:07 +0900

    Add files via upload

--- vege_db/dbcon.php
+++ vege_db/dbcon.php
@@ -1,7 +1,7 @@
 <?php
     $db_host="localhost";
     $db_user="root";
-    $db_password="1234";
+    $db_password="";
     $db_name="vege";
 
     $con=mysqli_connect($db_host, $db_user, $db_password, $db_name);
